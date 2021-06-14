0afc0c69cfec79c10931410b735f7d5785165ef8
unknown <kun_duc123@yahoo.com.vn>
2021-06-14 22:05:13 +0700

    Add front end

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(passport.initialize());
 
 app.use(cors({
@@ -28,6 +29,10 @@ app.use(cors({
 }));
 
 app.use('/api', apiRouter)
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
